71f7474d06dc112e5243defc5be7eb412430d38d
Краснова Татьяна <redtanya@yandex.ru>
2022-09-11 12:42:38 +0300

    add schema

--- src/test/java/ru/netology/test/BankTest.java
+++ src/test/java/ru/netology/test/BankTest.java
@@ -1,7 +1,6 @@
 package ru.netology.test;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
@@ -12,10 +11,6 @@ import static com.codeborne.selenide.Selenide.open;
 import static ru.netology.data.SQLHelper.cleanDatabase;
 
 public class BankTest {
-    @BeforeEach
-    void setup() {
-        open("http://localhost:9999", LoginPage.class);
-    }
 
     @AfterAll
     static void teardown() {
@@ -25,8 +20,9 @@ public class BankTest {
     @Test
     @DisplayName("Should successfull login to dashboard with exist login and password from sut test data")
     void shouldSuccesfullLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
         var authInfo = DataHelper.getAuthInfoWithTestData();
-        var verificationPage = LoginPage.validLogin(authInfo);
+        var verificationPage = loginPage.validLogin(authInfo);
         verificationPage.verifyVerificationPageVisiblity();
         var verificationCode = SQLHelper.getVerificationCode();
         verificationPage.validVerify(verificationCode.getCode());
